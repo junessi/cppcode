af2e95fd7cfcc9f069b512669e02676348511b15
Junlin Huang <cricketlong@gmail.com>
2022-11-15 00:25:22 +0100

    add algorithm arbiter

--- src/cppcode/common/sub_string.hpp
+++ src/cppcode/common/sub_string.hpp
@@ -4,7 +4,45 @@
 
 namespace cppcode { namespace common {
 
-int32_t sub_string(const std::string& s, const std::string& needle)
+enum class SUB_STRING_ALGORITHM : uint8_t
+{
+    BRUTE_FORCE,
+    KMP
+};
+
+int32_t sub_string_brute_force(const std::string& s, const std::string& needle);
+int32_t sub_string_KMP(const std::string& s, const std::string& needle);
+
+int32_t sub_string(const std::string& s, const std::string& needle, const SUB_STRING_ALGORITHM sub_string_algorithm = SUB_STRING_ALGORITHM::BRUTE_FORCE)
+{
+    switch (sub_string_algorithm)
+    {
+        case SUB_STRING_ALGORITHM::BRUTE_FORCE:
+            break;
+        case SUB_STRING_ALGORITHM::KMP:
+            return sub_string_KMP(s, needle);
+        default:
+            break;
+    }
+
+    return sub_string_brute_force(s, needle);
+}
+
+int32_t sub_string_brute_force(const std::string& s, const std::string& needle)
+{
+    auto* p = s.data();
+    for (size_t i = 0; i < (s.size() - needle.size() + 1); i++)
+    {
+        if (std::string_view{p++, needle.size()} == needle)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int32_t sub_string_KMP(const std::string& s, const std::string& needle)
 {
     std::vector<uint32_t> lps(needle.size(), 0);
 
--- test/common/subString/subStringTest.cpp
+++ test/common/subString/subStringTest.cpp
@@ -5,13 +5,19 @@
 
 TEST(subString, sub_string)
 {
+    using namespace cppcode::common;
+
     std::string s = "ABCABABCABCD";
     std::string needle = "ABCABCD";
-    EXPECT_EQ (5, cppcode::common::sub_string(s, needle));
+    EXPECT_EQ (5, sub_string(s, needle));
+    EXPECT_EQ (5, sub_string(s, needle, SUB_STRING_ALGORITHM::BRUTE_FORCE));
+    EXPECT_EQ (5, sub_string(s, needle, SUB_STRING_ALGORITHM::KMP));
 
     s = "ABCABABABCD";
     needle = "ABCABCD";
-    EXPECT_EQ (-1, cppcode::common::sub_string(s, needle));
+    EXPECT_EQ (-1, sub_string(s, needle));
+    EXPECT_EQ (-1, sub_string(s, needle, SUB_STRING_ALGORITHM::BRUTE_FORCE));
+    EXPECT_EQ (-1, sub_string(s, needle, SUB_STRING_ALGORITHM::KMP));
 }
 
 int main(int argc, char **argv) {
